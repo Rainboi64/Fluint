a7bfc01f865ec7641ead16919508ebb879021894
Yaman Alhalabi <yaman.al.halabi1@gmail.com>
2021-04-21 17:09:57 +0300

    Added Tests Projects.

---
+++ Fluint.Layer.Tests/Tests.fs
@@ -0,0 +1,11 @@
+namespace Fluint.Layer.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
