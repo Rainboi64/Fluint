18ec170ea3298bc348bc17c86ee4c2790044e492
Rainboi64 <yaman.al.halabi1@gmail.com>
2022-08-20 13:25:05 +0300

    Removed dockfx project.
    Signed-off-by: Rainboi64 <yaman.al.halabi1@gmail.com>

--- docfx_project/src/Fluint.Layer/Mathematics/README.md
+++
@@ -1,1 +0,0 @@
-# Inspired by: SHARPDX
--- docfx_project/src/Fluint.Layer/SDK/ILambdaListener.cs
+++
@@ -1,9 +0,0 @@
-namespace Fluint.Layer.SDK
-{
-    [Initialization(InitializationMethod.Scoped)]
-    public interface ILambdaListener : IModule
-    {
-        void Execute(string command);
-        void Listen();
-    }
-}
