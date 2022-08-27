d2499e782c4becc94aea58ca47d0390a8490e625
Rainboi64 <yaman.al.halabi1@gmail.com>
2022-08-27 19:23:04 +0300

    Added Fluint.Editor.Base.csproj
    Signed-off-by: Rainboi64 <yaman.al.halabi1@gmail.com>

---
+++ src/Fluint.Editor.Base/EditPolygon2D.cs
@@ -0,0 +1,19 @@
+﻿using Fluint.Layer.Editor;
+using Fluint.Layer.Mathematics;
+
+namespace Fluint.Editor.Base;
+
+public class EditPolygon : IEditPolygon2D
+{
+    public void Load(Vector3[] points)
+    {
+    }
+
+    public void Update()
+    {
+    }
+
+    public void Render()
+    {
+    }
+}
