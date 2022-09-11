7bbf019ec3b9a05606962b049331c764563b7a8f
Rainboi64 <yaman.al.halabi1@gmail.com>
2022-09-11 23:12:09 +0300

    Implement Filesystem VFS.
    Signed-off-by: Rainboi64 <yaman.al.halabi1@gmail.com>

---
+++ src/Fluint.VirtualFileSystem.Base/File.cs
@@ -0,0 +1,37 @@
+// 
+// File.cs
+// 
+// Copyright (C) 2022 Yaman Alhalabi
+
+using System;
+using Fluint.Layer.VirtualFileSystem;
+
+namespace Fluint.VirtualFileSystem.Base;
+
+public class File : IFile
+{
+    private byte[] _bytes;
+
+    public File(byte[] bytes)
+    {
+        Bytes = bytes;
+    }
+
+    public void Update(string file)
+    {
+        OnChange?.Invoke(this, new FileChangedEvent());
+        Bytes = System.IO.File.ReadAllBytes(file);
+    }
+
+    public byte[] Bytes
+    {
+        get => _bytes;
+        private set
+        {
+            _bytes = value;
+            OnChange?.Invoke(this, new FileChangedEvent());
+        }
+    }
+
+    public event EventHandler<FileChangedEvent> OnChange;
+}
